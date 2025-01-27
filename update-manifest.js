9242dec2adbd08809b8cc7b53c3ba420e1ec02b6
Ismoil Hamidzoda <ismoil2888@gmail.com>
2025-01-27 22:57:47 +0500

    icons pwa

--- update-manifest.js
+++ update-manifest.js
@@ -42,6 +42,23 @@ const path = require("path");
 
 const manifestPath = path.join(__dirname, "web-build", "manifest.json");
 const indexPath = path.join(__dirname, "web-build", "index.html");
+const pwaIconsSrc = path.join(__dirname, "pwa-icons");
+const pwaIconsDest = path.join(__dirname, "web-build", "icons");
+
+// Копирование папки pwa-icons в web-build/icons
+const copyFolder = (src, dest) => {
+  fs.mkdirSync(dest, { recursive: true }); // Создаём целевую папку, если она не существует
+  fs.readdirSync(src).forEach((file) => {
+    const srcPath = path.join(src, file);
+    const destPath = path.join(dest, file);
+
+    if (fs.lstatSync(srcPath).isDirectory()) {
+      copyFolder(srcPath, destPath); // Рекурсивное копирование подпапок
+    } else {
+      fs.copyFileSync(srcPath, destPath); // Копирование файлов
+    }
+  });
+};
 
 // Обновление manifest.json
 fs.readFile(manifestPath, "utf8", (err, data) => {
@@ -55,6 +72,20 @@ fs.readFile(manifestPath, "utf8", (err, data) => {
     manifest.prefer_related_applications = false; // Изменяем prefer_related_applications
     manifest.start_url = "/Intikhobot-App-IOS/?utm_source=web_app_manifest"; // Обновляем start_url
     manifest.theme_color = "#3F0DD1"; // Устанавливаем theme_color
+
+    // Добавляем иконки
+    manifest.icons = [
+      {
+        src: "/icons/int_pwa_icon_512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+      {
+        src: "/icons/int_pwa_icon_1024.png",
+        sizes: "1024x1024",
+        type: "image/png",
+      },
+    ];
 
     fs.writeFile(manifestPath, JSON.stringify(manifest, null, 2), "utf8", (err) => {
       if (err) {
@@ -88,3 +119,11 @@ fs.readFile(indexPath, "utf8", (err, data) => {
     }
   });
 });
+
+// Копируем папку pwa-icons в web-build/icons
+try {
+  copyFolder(pwaIconsSrc, pwaIconsDest);
+  console.log("Папка pwa-icons успешно скопирована в web-build/icons!");
+} catch (copyErr) {
+  console.error("Ошибка при копировании pwa-icons:", copyErr);
+}
